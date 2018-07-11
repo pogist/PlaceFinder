329039dde6e0669951d89981ed3d3d440f0290cf
pogist <murilo.paixao.2@gmail.com>
2018-07-11 15:16:56 -0300

    Initial Commit

---
+++ PlaceFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaceFinder
+//
+//  Created by Murilo da Paixão on 11/07/2018.
+//  Copyright © 2018 Murilo da Paixão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PlaceFinderTests/PlaceFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceFinderTests.swift
+//  PlaceFinderTests
+//
+//  Created by Murilo da Paixão on 11/07/2018.
+//  Copyright © 2018 Murilo da Paixão. All rights reserved.
+//
+
+import XCTest
+@testable import PlaceFinder
+
+class PlaceFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
