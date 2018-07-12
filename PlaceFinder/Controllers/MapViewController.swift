adf40b3b5c4ebaa706253baa1c248746ac39a96c
pogist <murilo.paixao.2@gmail.com>
2018-07-12 13:20:28 -0300

    Points map to user's current location

--- PlaceFinder/Controllers/MapViewController.swift
+++ PlaceFinder/Controllers/MapViewController.swift
@@ -7,18 +7,47 @@
 //
 
 import UIKit
+import CoreLocation
 import GoogleMaps
 
 class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: GMSMapView!
     
-    lazy var mapViewModel = MapViewModel()
+    var mapViewModel: MapViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.mapViewModel = MapViewModel()
+        mapViewModel.locationManager = CLLocationManager()
+        
+        mapViewModel.delegate = self
+        mapViewModel.requestAuthorizationForLocationService()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
+    }
+}
+
+extension MapViewController: MapViewModelDelegate {
+    
+    func didUpdateCameraPosition(_ cameraPosition: GMSCameraPosition?) {
+        guard let cameraPosition = cameraPosition else {
+            return
+        }
+        
+        UIView.animate(
+            withDuration: 0.5,
+            animations: { [weak self] in
+                self?.mapView.animate(to: cameraPosition)
+            },
+            completion: { [weak self] _ in
+                self?.mapView.camera = cameraPosition
+            })
     }
 }
---
+++ PlaceFinderTests/MapViewControllerTests.swift
@@ -0,0 +1,40 @@
+//
+//  MapViewControllerTests.swift
+//  PlaceFinderTests
+//
+//  Created by Murilo da Paixão on 12/07/2018.
+//  Copyright © 2018 Murilo da Paixão. All rights reserved.
+//
+
+import XCTest
+import GoogleMaps
+@testable import PlaceFinder
+
+class MapViewControllerTests: XCTestCase {
+    
+    var controllerUnderTest: MapViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        controllerUnderTest = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateInitialViewController()! as! MapViewController
+        _ = controllerUnderTest.view
+    }
+    
+    override func tearDown() {
+        controllerUnderTest = nil
+        super.tearDown()
+    }
+    
+    func testDidUpdateWithNilCameraPosition() {
+        // Given
+        let cameraBeforeTheUpdate = controllerUnderTest.mapView.camera
+        
+        // When
+        controllerUnderTest.didUpdateCameraPosition(nil)
+        
+        // Then
+        let cameraAfterTheUpdate = controllerUnderTest.mapView.camera
+        XCTAssertEqual(cameraBeforeTheUpdate, cameraAfterTheUpdate)
+    }
+}
